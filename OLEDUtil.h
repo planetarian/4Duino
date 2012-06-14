1d0c1f89d9da2094ada3db9276e5c42f579d5d66
Ash Wolford <piro@live.com>
2012-06-13 20:43:06 -0500

    Removed BlinkLED and CheckMemory -- these aren't relevant to the library

--- OLEDUtil.h
+++ OLEDUtil.h
@@ -11,17 +11,14 @@ class OLEDUtil
 public:
     static uint8_t getByte(uint32_t value, uint8_t zeroBasedByteFromRight = 0);
     static bool readHexAsDec(uint8_t value, uint8_t& result);
-    static void blinkLED(uint8_t pin, uint8_t numTimes, uint16_t blinkDelayMs = 100);
-    static String byteToString(uint8_t data);
-    static String shortToString(uint16_t data);
-    static String longToString(uint32_t data);
-    static float analogToVoltage(uint16_t value, float refVoltage = 5.0);
-    static String analogToVoltageString(uint16_t value, float refVoltage = 5.0);
+    static String byteToString(uint8_t data); // "0x12"
+    static String shortToString(uint16_t data); // "0x1234"
+    static String longToString(uint32_t data); // "0x12345678"
+    static float analogToVoltage(uint16_t value, float refVoltage = 5.0); // e.g. 4.98f
+    static String analogToVoltageString(uint16_t value, float refVoltage = 5.0); // e.g. "4.98"
     static String floatToString(float value, uint8_t decimalPlaces = 2);
-    static uint32_t scaleAnalog(uint32_t value, uint32_t max);
+    static uint32_t scaleAnalog(uint32_t value, uint32_t targetScale); // shortcut to convertValueScale(value, 1024, targetScale)
     static uint32_t convertValueScale(uint32_t value, uint32_t valueScale, uint32_t targetScale);
-    static uint16_t checkMemory();
-
 private:
     OLEDUtil();
     ~OLEDUtil();
